4a5173f968996088c99e017eeb7746dc0fac1637
nguyenxuantruong08032005-tech <nguyenxuantruong08032005@gmail.com>
2025-10-30 15:04:00 +0700

    chore: init TapoPOS WinForms .NET Framework 4.8

---
+++ TapoPOS/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace TapoPOS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtTenDN = new System.Windows.Forms.TextBox();
+            this.txtMatKhau = new System.Windows.Forms.TextBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(55, 99);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(120, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Tên đăng nhập:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(69, 158);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Mật khẩu:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // txtTenDN
+            // 
+            this.txtTenDN.Location = new System.Drawing.Point(192, 93);
+            this.txtTenDN.Name = "txtTenDN";
+            this.txtTenDN.Size = new System.Drawing.Size(211, 26);
+            this.txtTenDN.TabIndex = 2;
+            // 
+            // txtMatKhau
+            // 
+            this.txtMatKhau.Location = new System.Drawing.Point(192, 152);
+            this.txtMatKhau.Name = "txtMatKhau";
+            this.txtMatKhau.Size = new System.Drawing.Size(176, 26);
+            this.txtMatKhau.TabIndex = 3;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(73, 252);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(116, 41);
+            this.btnLogin.TabIndex = 4;
+            this.btnLogin.Text = "Đăng nhập";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(340, 252);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(116, 41);
+            this.btnCancel.TabIndex = 5;
+            this.btnCancel.Text = "Hủy";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(529, 358);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.txtMatKhau);
+            this.Controls.Add(this.txtTenDN);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtTenDN;
+        private System.Windows.Forms.TextBox txtMatKhau;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Button btnCancel;
+    }
+}
+
---
+++ TapoPOS/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TapoPOS
+{
+    public partial class Form1 : Form
+    {
+        private int loginAttempts = 0; // Đếm số lần nhập sai
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            //ktr đầy đủ
+            if (txtTenDN.Text == string.Empty)
+            {
+                MessageBox.Show("Hãy nhập vào tài khoản", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                txtTenDN.Focus();
+                return;
+            }
+            if (txtMatKhau.Text == string.Empty)
+            {
+                MessageBox.Show("Hãy nhập vào mật khẩu", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                txtMatKhau.Focus();
+                return;
+            }
+            //ktr tên đăng nhập có hợp lệ không
+            string connString = "Data Source=DESKTOP-1OUP7MA\\SQLEXPRESS;Initial Catalog=QLCUAHANGTAPHOA;Integrated Security=True;Encrypt=False";
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                try
+                {
+                    conn.Open();
+                    string query = "SELECT MatkhauHash FROM [NguoiDung] WHERE TenDangNhap = @TenDangNhap";
+                    SqlCommand cmd = new SqlCommand(query, conn);
+                    cmd.Parameters.AddWithValue("@TenDangNhap", txtTenDN.Text);
+
+                    object result = cmd.ExecuteScalar();
+
+                    if (result == null)
+                    {
+                        MessageBox.Show("Không có ID này!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        loginAttempts++;
+                    }
+                    else if (result.ToString() != txtMatKhau.Text)
+                    {
+                        MessageBox.Show("Sai mật khẩu!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        loginAttempts++;
+                    }
+                    else
+                    {
+                        // Lấy loại nhân viên
+                        string loaiQuery = "SELECT TenDangNhap FROM [NguoiDung] WHERE TenDangNhap = @TenDangNhap";
+                        SqlCommand loaiCmd = new SqlCommand(loaiQuery, conn);
+                        loaiCmd.Parameters.AddWithValue("@TenDangNhap", txtTenDN.Text);
+                        object loaiResult = loaiCmd.ExecuteScalar();
+                        string LoaiNV = loaiResult != null ? loaiResult.ToString() : "Không rõ";
+
+                        MessageBox.Show("Đăng nhập thành công!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                        //// Truyền loại nhân viên sang form chính
+                        //SweetMomentsApp mainForm = (SweetMomentsApp)Application.OpenForms["SweetMomentsApp"];
+                        //if (mainForm != null)
+                        //{
+                        //    mainForm.XuLyDangNhapThanhCong(LoaiNV); // Truyền LoaiNV
+                        //}
+
+                        this.Close(); // Đóng form đăng nhập
+                        return;
+                    }
+
+                    if (loginAttempts >= 3)
+                    {
+                        MessageBox.Show("Bạn đã nhập sai quá 3 lần, hệ thống sẽ tự động đóng!", "Cảnh báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                        this.Close();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Lỗi kết nối CSDL: " + ex.Message, "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
